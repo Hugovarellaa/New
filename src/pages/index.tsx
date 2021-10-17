599caf809404372bea99a0d4b9747541472da476
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-17 10:30:53 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +8,22 @@ export default function Home() {
       <Head>
         <title>Home | Ig.news</title>
       </Head>
-      
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
