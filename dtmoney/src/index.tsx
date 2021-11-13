d4d8753a467f187a7642549f234b6cbdbdfeab3b
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-13 18:33:07 -0300

    Chapter II
     3 / 5 completed

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,3 +1,23 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => ({
+      lista: [
+        {
+          id: 1,
+          title: "transaction 1",
+          amount: 100,
+          type: "deposit",
+          category: "Food",
+          createAt: new Date(),
+        },
+      ],
+    }));
+  },
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
