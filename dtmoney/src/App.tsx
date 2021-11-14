651af0b132c6020c58645c20502db7888226b1de
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-13 23:36:09 -0300

    Chapter II
    3 / 5 - Configurando modal de criação

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,13 +1,33 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalIsOpen, setIsNewTransactionModalIsOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalIsOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalIsOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+
+      <Modal
+        isOpen={isNewTransactionModalIsOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }
