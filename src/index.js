e39e2708df008f75f2b21a8a19c005e86e8954d3
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-25 10:56:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,39 +3,39 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
+
 const customers = [];
-// cpf - string
-// name - string
-// id - uuuid
-// statemet - []
+
+//cpf - string
+//name - string
+// id - uuid
+// statement - []
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-
-  const customersAlreadyExists = customers.some(
+  const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
-  if (customersAlreadyExists) {
-    return response.status(400).json({ error: "Customer already exists!" });
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
   }
 
   customers.push({
     cpf,
     name,
     id: uuidv4(),
-    statemet: [],
+    statement: [],
   });
   return response.status(201).send();
 });
 
 app.get("/statement/:cpf", (request, response) => {
   const { cpf } = request.params;
-  const customer = customers.find(customer => customer.cpf === cpf);
-  
-  if(!customer){
-    return response.status(400).send({error: "Customer not found"});
-  }
 
-
+  const customer = customers.find(
+    customer => customer.cpf === cpf
+  )
   return response.json(customer.statement)
 });
 
