257f6d247b9e0f2e2e5807aee924021ffc92352c
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-25 12:50:34 -0300

    Chapter I
     - Conhecendo os requisitos da aplicação
     - Cadastro de conta
     - Validando CPF existente
     - Listando extrato
     - Validando a conta
     - Middlewares
     - Criando depósito na conta
     - Criando saque na conta
     - Listar extrato bancário por data

--- src/index.js
+++ src/index.js
@@ -19,11 +19,16 @@ function verifyExistsAccountCPF(request, response, next) {
   request.customer = customer;
   return next();
 }
-
-//cpf - string
-//name - string
-// id - uuid
-// statement - []
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -62,6 +67,33 @@ app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
 
   customer.statement.push(statementOperation);
   return response.status(201).send();
+});
+
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+  const statementOperation = {
+    amount,
+    create_at: new Date(),
+    type: "debit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+  const statement = customer.statement.filter((statement) => statement.create_at.toDateString() === new Date(dateFormat).toDateString());
+
+  return response.json(statement);
 });
 
 app.listen(3333);
