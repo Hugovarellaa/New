b5fd3199d7663c0e993ffa3d2fc53d541c5bff33
Hugo <hugovarellaa@gmail.com>
2022-01-26 10:23:01 -0300

    Criando middleware

--- src/server.js
+++ src/server.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ erro: "Customer not found" });
+  }
+  req.customer = customer;
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -27,14 +41,8 @@ app.post("/account", (req, res) => {
   return res.status(201).json(customers);
 });
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((cust) => cust.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ erro: "Customer not found" });
-  }
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
   return res.json(customer.statement);
 });
 
