adb8a3b1e5c13416cc14e772565ef52e587c7050
Hugo <hugovarellaa@gmail.com>
2022-01-26 13:01:48 -0300

    End

--- src/server.js
+++ src/server.js
@@ -131,4 +131,12 @@ app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
   return res.status(200).json(customers);
 });
 
+app.get("/balance", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+});
+
 app.listen(3333, () => console.log("Server is running in port 3333"));
