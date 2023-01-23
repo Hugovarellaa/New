d654bf7d291afeb953ffd9346f2a09a282fcdfa2
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 10:59:26 -0300

    choro: Create account

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const PORT = 3333;
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello World! ok');
+const customers = [];
+
+app.post('/account', (request, response) => {
+	const { cpf, name } = request.body;
+
+	const account = {
+		id: uuidV4(),
+		cpf,
+		name,
+		statement: [],
+	};
+
+	customers.push(account);
+
+	return response.status(201).json({ account });
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
