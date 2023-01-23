e994323ff6e0a21f4e27a7c287c5664533564dd1
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 10:31:13 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 3333;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World! ok");
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
