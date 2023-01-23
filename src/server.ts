8cb3d08748e904a0b2190766cbbbf1407a7dd1fa
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-23 12:03:36 -0300

    choro: deposit router

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { v4 as uuidV4 } from 'uuid';
 
 const PORT = 3333;
 const app = express();
 app.use(express.json());
 
-const customers = [];
+interface ICustomer {
+	id: string;
+	cpf: string;
+	name: string;
+	statement: {
+		description: string;
+		amount: number;
+		type: 'credit' | 'debit'; // deposit
+		created_at: Date;
+	}[];
+}
 
-function verifyAccountIfExistsWithCPF(request, response, next) {
+const customers: ICustomer[] = [];
+
+function verifyAccountIfExistsWithCPF(
+	request: Request,
+	response: Response,
+	next: NextFunction,
+) {
 	const { cpf } = request.headers;
 
 	const customer = customers.find((customer) => customer.cpf === cpf);
@@ -15,6 +31,7 @@ function verifyAccountIfExistsWithCPF(request, response, next) {
 	if (!customer) {
 		return response.status(400).json({ message: 'Customer not found' });
 	}
+
 	request.customer = customer;
 	return next();
 }
@@ -30,7 +47,7 @@ app.post('/account', (request, response) => {
 		return response.status(400).json({ message: 'Account already exists' });
 	}
 
-	const account = {
+	const account: ICustomer = {
 		id: uuidV4(),
 		cpf,
 		name,
@@ -45,6 +62,22 @@ app.post('/account', (request, response) => {
 app.get('/statement', verifyAccountIfExistsWithCPF, (request, response) => {
 	const { customer } = request;
 	return response.json({ customer: customer.statement });
+});
+
+app.post('/deposit', verifyAccountIfExistsWithCPF, (request, response) => {
+	const { customer } = request;
+	const { description, amount } = request.body;
+
+	const operation = {
+		description,
+		amount,
+		type: 'credit', // deposit
+		created_at: new Date(),
+	};
+
+	customer.statement.push(operation);
+
+	return response.status(201).send();
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
